4f8dd82e03a587eb02ed489bb83b83bba9c8962a
unknown <gownideepu23@gmail.com>
2023-04-06 13:14:36 +0530

    added

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,35 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException{
+        String username=req.getParameter("user");
+        String password= req.getParameter("password");
+
+        if(username.equals("admin") && password.equals("password")){
+            HttpSession session=req.getSession();
+            session.setAttribute(username,username);;
+            try{
+                res.sendRedirect("success.jsp");
+            }
+            catch (IOException e){
+                throw new RuntimeException(e);
+            }
+        }
+        else{
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/Login.html");
+            PrintWriter out = res.getWriter();
+            out.println("<font color=red> Incorrect credentials </font>");
+            rd.include(req,res);
+        }
+    }
+}
